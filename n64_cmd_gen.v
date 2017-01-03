3dd8db7ac63988b49e25ab6761a8f416feb28912
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2017-01-03 19:27:30 +0100

    Added package pin buffering for tristate output
    Now gamepad replies to read command

--- n64_cmd_gen.v
+++ n64_cmd_gen.v
@@ -1,7 +1,9 @@
-module n64_cmd_gen(input wire clk, input wire trigger, output wire dout);
+module n64_cmd_gen(input wire clk, input wire trigger, output wire enable_o, output wire dout);
 
 //-- Cable entre contador y registro de desplazamiento
 wire ctr_empty;
+
+assign enable_o = ~ctr_empty;
 
 //-- Instanciar el contador y establecer el valor del inicial
 counterM #(.M(36))
@@ -12,7 +14,7 @@ counterM #(.M(36))
   );
 
 //-- Instanciar el registro de desplazamiento y establecer el valor del inicial
-shiftM #(.M(36), .INI(36'b00010001000100010001000100010111011z))
+shiftM #(.M(36), .INI(36'b000100010001000100010001000101110111))
     shift32(
       .clk(clk),
       .enable(~ctr_empty),
--- n64_read_loop.v
+++ n64_read_loop.v
@@ -1,15 +1,21 @@
-module n64_read_loop(input wire clk, output wire dout);
+module n64_read_loop(input wire clk, inout dout);
+
+//-- Entrada a usar en nuestro circuito, con el pull-up activado
+wire dout_o;
+wire dout_i;
 
 //-- Cable entre contador y registro de desplazamiento
 wire read_start;
 wire clk_1M;
+wire output_en;
 
 //-- Instanciar el registro de desplazamiento y establecer el valor del inicial
 //-- Instanciar el componente y establecer el valor del divisor
 n64_cmd_gen n64 (
     .clk(clk_1M),
     .trigger(read_start),
-    .dout(dout)
+    .enable_o(output_en),
+    .dout(dout_o)
   );
 
 divM div12 (
@@ -21,4 +27,15 @@ triggerM trigger512 (
     .clk(clk_1M),
     .trigger(read_start)
     );
+
+SB_IO #(
+    .PIN_TYPE(6'b 1010_01),
+    .PULLUP(1'b 1)
+) io_pin (
+    .PACKAGE_PIN(dout),
+    .OUTPUT_ENABLE(output_en),
+    .D_OUT_0(dout_o),
+    .D_IN_0(dout_i)
+);
+
 endmodule
